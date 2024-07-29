92041cfc0c37003a3bee3a2705155bee1443e093
Pragya <pragya142018@gmail.com>
2024-07-29 16:46:44 +0530

    adding more functionalities

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,13 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-     path('about/',views.about,name='about'),
-      path('contact/',views.contact,name='contact'),
+path('admin/', admin.site.urls),
+path('',views.home,name='home'),
+ path('about/',views.about,name='about'),
+ path('contact/',views.contact,name='contact'),
 
     #   making the controoler for chai app
-          path('chai/',include('chai.urls')),
-]
+ path('chai/',include('chai.urls')),
+
+ path("__reload__/", include("django_browser_reload.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
